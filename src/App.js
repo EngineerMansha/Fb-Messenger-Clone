fef6a7e2193b2fb833a41d34082fd00621b3a12d
EngineerMansha <manshauaf@gmail.com>
2022-06-25 02:04:55 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import { Button,FormControl,InputLabel,Input } from '@material-ui/core';
 import Messages from './Components/Messages';
@@ -6,26 +6,32 @@ import Messages from './Components/Messages';
 
 function App() {
   const [input, setinput] = useState('');
-  const [message, setMessages] = useState(['Engineer Mansha','Hamza','UAF',]);
-  console.log(input)
-  console.log(message)
+  const [message, setMessages] = useState([{ username: 'Mansha', text: 'Hello' }, { username: 'Hamza', text: 'Hi Hamza' },]);
+  const [username, setUsername] = useState('');
+
+
+  useEffect(() => {
+    setUsername(prompt('Please Enter Your name'));
+  }, [])
+  
   const sendmsg = (event) => {
     event.preventDefault();
     //all the logic send msg go to 
-    setMessages([ ...message,input])
+    setMessages([ ...message,{username:username, text:input}])
     setinput('');
   }
-  
+  console.log(message);
 
   return (
     <div className="App">
-      <h1>Fb Clone</h1>
+      <h2>Facebook Messenger Clone</h2>
+      <h2>Welcome { username}</h2>
       <form>
         <FormControl>
           <InputLabel> Enter the message...</InputLabel>
           <Input value={input} onChange={event => setinput(event.target.value)} />
           <br></br>
-        <Button  variant="contained" color="primary" type='submit' onClick={sendmsg}>Send Message</Button>
+        <Button  disabled={!input} variant="contained" color="primary" type='submit' onClick={sendmsg}>Send Message</Button>
         </FormControl>
      
       </form>
@@ -33,7 +39,7 @@ function App() {
     
       {
         message.map(message => (
-          <Messages text={message} />
+          <Messages username={message.username} text={message.text} />
          
         ))
         
